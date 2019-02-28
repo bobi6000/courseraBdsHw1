7f5a7e350a6c9cf765a1951106805b974706ce99
paweł.staniak <pawel.staniak@apsenergia.pl>
2019-02-28 11:51:31 +0100

    init repo

---
+++ check_brackets.c
@@ -0,0 +1,28 @@
+/*
+ * check_brackets.c
+ *
+ *  Created on: 28 lut 2019
+ *      Author: pawe³.staniak
+ */
+
+#include <stdio.h>
+#include <stdbool.h>
+
+
+
+
+
+int main (void){
+
+	char c=0;
+
+
+	while ((c=getchar())!=EOF){
+
+		printf("%c \n",c);
+
+	}
+
+
+	return 0;
+}
